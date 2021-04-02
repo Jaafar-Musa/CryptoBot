4b36789859f75b8b28b284e173dd6316b05f27ae
Jaafar-Musa02 <jaafarmusa.jm02@gmail.com>
2021-04-02 00:45:25 +0000

    changes to setup

--- cryptoBot.py
+++ cryptoBot.py
@@ -1,14 +1,25 @@
-from telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Bot, Update, Message
+from telegram.ext import Updater, CommandHandler, CallbackContext, Filters,MessageHandler
 from decouple import config
 import cryptoBot_func
 
-bot = Bot(config('TELEGRAM_API_URI'))
+def response(update:Update, context:CallbackContext):
+    if 'hi' in update.message.text:
+        update.message.reply_text('hiiiii')
+    
 
-updater = Updater(config('TELEGRAM_API_URI'))
+def help(update:Update, context:CallbackContext): 
+    update.message.reply_text('helppppp')
 
-updater.dispatcher.add_handler(CommandHandler('hello', cryptoBot_func.hello))
-updater.dispatcher.add_handler(CommandHandler('Owned', cryptoBot_func.owned_crypto))
 
-updater.start_polling()
-updater.idle()
+def main():
+    updater = Updater(config('TELEGRAM_API_URI'))
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('help',help))
+    dispatcher.add_handler(MessageHandler(Filters.text,response))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
--- cryptoBot_func.py
+++ cryptoBot_func.py
@@ -5,19 +5,43 @@ from db import db
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-# def test():
-#     test= {
-#         "test1":"Test"
-#     }
-#     crypto = db.owned_crypto
-#     crypto_id = crypto.insert_one(test)
-#     print('Added')
 
 def bought_crypto():
+    # user gives the token, and the price its bought at
+    #  calculates and returns dca
+    pass
+
+
+def sold_crypto():
+    # user gives the token, and the price its sold at
+    # bot calc the profit. if + then DCA the bought at price for said crypto
+    # returns profit from sale
+
+    pass
+
+
+def profit_from():
+    # returns profits from individual tokens
+    #? $ADA +$20 up by x%
+    pass
+
+
+def total_profit():
+    # returns total profit
+    #? +20 x%
+    pass
+
+
+def portfolio():
+    # returns percentages of crypto holdings
+    #? $ADA 60% $LINK 40%
     pass
 
 
 def owned_crypto(update: Update, context: CallbackContext):
+    # Returns all owned crypto
+    msg = update.message.text
+    print(msg)
     crypto = db.owned_crypto
     data = crypto.find()
     arr = []
